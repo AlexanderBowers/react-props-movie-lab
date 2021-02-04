728aea5665edbab3d69de686bdd316c57baad7f4
AlexanderBowers <7319abowers@gmail.com>
2021-02-04 11:09:17 -0800

    this lab sucks

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -27,17 +27,27 @@ const posterMap = {
   'default': defaultPoster
 }
 
+
+
+
 export default class MovieCard extends Component {
 
   render() {
     return (
       <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster= {posterMap[this.props.poster]} />
+        <CardBack title= {this.props.title} genres= {this.props.genres} IMDBRating= {this.props.IMDBRating}/>
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+CardFront.defaultProps= {
+  poster: "default"
+}
+CardBack.defaultProps={
+  title: "Unknown",
+  IMDBRating: "null",
+  genres: "No Genre(s) Found"
+}
